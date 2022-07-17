4678b00a0785b2a8b5130255ed54bce5ad979b44
Abin-S-Yaseen <abinchingeli12@gmail.com>
2022-07-17 19:02:54 +0530

    Update problem 1.py

--- problem 1.py
+++ problem 1.py
@@ -1,2 +1,6 @@
+# Here is a sample line of code that can be executed in Python:
+# print("Hello, World!")
+# The above code will print Hello, World! on your screen.Try it yourself in the editor below!
+
 if __name__ == '__main__':
     print("Hello, World!")
