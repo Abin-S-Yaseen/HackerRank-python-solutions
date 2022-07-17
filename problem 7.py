b3a1b363ccb7bad476d5b37de2d9e80017afb8b7
Abin-S-Yaseen <abinchingeli12@gmail.com>
2022-07-17 19:03:23 +0530

    Update problem 7.py

--- problem 7.py
+++ problem 7.py
@@ -1,3 +1,7 @@
+# The included code stub will read an integer,n,from STDIN.
+# Without using any string methods, try to print the following:
+# 1,2,3...n
+
 from __future__ import print_function
 
 if __name__ == '__main__':
